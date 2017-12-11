259239a56618b00e7df93aa618784b46c15e84e7
Becky Smith <becky.smith@ecometrica.com>
2017-12-11 14:33:43 +0000

    Update version and add changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-hashedfilenamestorage',
-    version='1.0',
+    version='2.0',
     description=('A Django storage backend that names files by hash value.'),
     long_description=open('README.rst', 'r').read(),
     author='Ecometrica',
